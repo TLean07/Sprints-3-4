aef820b232dac00fb1101e36041cef1395de56d3
TLean07 <125459372+TLean07@users.noreply.github.com>
2025-09-24 22:49:00 -0300

    commit

---
+++ Passa-Bola/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.passabola.app',
+  appName: 'passa-bola',
+  webDir: 'dist'
+};
+
+export default config;
